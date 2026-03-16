ec384e2069e6211e65796cbc9672296fe8e42a89
Zoldok <max_kyrtimov@mail.ru>
2026-03-17 00:22:25 +0300

    add base

---
+++ components/TheFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TheFooter = () => {
+  return (
+    <footer className='container'>
+      Created by Zoldor
+    </footer>
+  );
+};
+
+export default TheFooter;
